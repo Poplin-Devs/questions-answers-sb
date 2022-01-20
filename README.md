65ef5cb6f5de75a74e1e74193b912800dbb54a0e
Samuel Beddow <beddo018@umn.edu>
2022-01-20 15:36:50 -0600

    amalgamated/deleted files, added basic readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # questions-answers-sb
+
+## About
+A microservice API for routing requests from MarbleWear (https://github.com/marble-systems/marblewear). Can be reconfigured for both vertical and horizontal deployments.
+
+## Installation
+* While in root directory (questions-answers-sb):
+```
+npm install
+```
+* In db/queries.js:
+```
+'mongodb://127.0.0.1:27017/questions-answers-sb' =>
+'${instanceUrl}'
+```
--- db/queries.js
+++ db/queries.js
@@ -1,50 +1,36 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
 mongoose.connect('mongodb://127.0.0.1:27017/questions-answers-sb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(db => console.log('DB is connected!'))
+  .then(() => console.log('DB is connected!'))
   .catch(err => console.log('DB connection failed with error: ', err));
-const { Schema } = mongoose;
 
-const schemas = {
-  questionSchema: new Schema({
-    id: Number,
-    product_id: Number,
-    question_body: String,
-    date_written: String,
-    asker_name: String,
-    asker_email: String,
-    answers: [{
-      question_id: Number,
-      answerer_email: String,
-      answerer_name: String,
-      body: String,
-      date_written: String,
-      helpful: Number,
-      id: Number,
-      reported: Boolean,
-      answer_photos: [{ id: Number, url: String }],
-    }],
-    reported: Boolean,
-    helpful: Number
-  }),
-
-  answerSchema: new Schema({
-    id: Number,
+const questionSchema = new Schema({
+  id: Number,
+  product_id: Number,
+  question_body: String,
+  date_written: String,
+  asker_name: String,
+  asker_email: String,
+  answers: [{
     question_id: Number,
-    answer_body: String,
-    date_written: String,
-    answerer_name: String,
     answerer_email: String,
-    answer_photos: [{ id: Number, url: String }],
+    answerer_name: String,
+    body: String,
+    date_written: String,
+    helpful: Number,
+    id: Number,
     reported: Boolean,
-    helpful: Number
-  })
-}
+    answer_photos: [{ id: Number, url: String }],
+  }],
+  reported: Boolean,
+  helpful: Number
+});
 
-const Question = mongoose.model('question', schemas.questionSchema);
-const Answer = mongoose.model('answer', schemas.answerSchema);
+const Question = mongoose.model('question', questionSchema);
 
 const Queries = {
   getQuestions: (product_id, callback) => {
